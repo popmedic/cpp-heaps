07528a34621c4772a8abed1f7837366a094b0556
kscardina <popmedic@gmail.com>
2018-05-12 14:09:08 -0600

    adding data range for random data generation

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "heaps.hpp"
 
 #define ARRAY_SIZE 10
+#define DATA_RANGE 100
 
 using namespace std;
 
@@ -13,7 +14,7 @@ int main(int argc, char const *argv[])
     int array[ARRAY_SIZE];
 
     srand(time(NULL));
-    for(int i = 0; i < ARRAY_SIZE; i++) array[i] = rand()%ARRAY_SIZE;
+    for(int i = 0; i < ARRAY_SIZE; i++) array[i] = rand()%DATA_RANGE;
     
     MinHeap *min_heap = new MinHeap(array[0]);
     for (int i = 1; i < ARRAY_SIZE; i++) min_heap->insert(new MinNode(array[i]));
