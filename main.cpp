723e4c2bba0f9571f1dbbc55a8bc0df247171ee2
kscardina <popmedic@gmail.com>
2018-05-12 14:04:57 -0600

    randomize the test array

--- main.cpp
+++ main.cpp
@@ -1,20 +1,28 @@
 #include<iostream>
+#include<cstdlib>
+#include<ctime>
+
 #include "heaps.hpp"
+
+#define ARRAY_SIZE 10
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int array[] = {10, 4, 8, 2, 1, 7, 9, 6, 5, 3};
+    int array[ARRAY_SIZE];
+
+    srand(time(NULL));
+    for(int i = 0; i < ARRAY_SIZE; i++) array[i] = rand()%ARRAY_SIZE;
     
     MinHeap *min_heap = new MinHeap(array[0]);
-    for (int i = 1; i < 10; i++) min_heap->insert(new MinNode(array[i]));
+    for (int i = 1; i < ARRAY_SIZE; i++) min_heap->insert(new MinNode(array[i]));
     cout << "Min Heap result:" << endl;
     min_heap->print();
     delete min_heap;
 
     MaxHeap *max_heap = new MaxHeap(array[0]);
-    for (int i = 1; i < 10; i++) max_heap->insert(new MaxNode(array[i]));
+    for (int i = 1; i < ARRAY_SIZE; i++) max_heap->insert(new MaxNode(array[i]));
     cout << "Max Heap result:" << endl;
     max_heap->print();
     delete max_heap;
