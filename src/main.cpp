3c85e8db58435afa715435a695a5479510a09904
kscardina <popmedic@gmail.com>
2018-05-14 08:39:59 -0600

    adding speed test

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,38 @@
 
 #include "heaps.hpp"
 
-#define ARRAY_SIZE 13
-#define DATA_RANGE 100
+#define ARRAY_SIZE 1000
+#define DATA_RANGE 10000
 
 using namespace std;
+
+int get_min_h(const int n, const int *ints) {
+    MinHeap *min_heap = new MinHeap(ints[0]);
+    for (int i = 1; i < n; i++) min_heap->insert(new MinNode(ints[i]));
+    int rtn = min_heap->data;
+    delete min_heap;
+    return rtn;
+}
+
+int get_max_h(const int n, const int *ints) {
+    MaxHeap *max_heap = new MaxHeap(ints[0]);
+    for (int i = 1; i < n; i++) max_heap->insert(new MaxNode(ints[i]));
+    int rtn = max_heap->data;
+    delete max_heap;
+    return rtn;
+}
+
+int get_min_a(const int n, const int *ints) {
+    int rtn = ints[0];
+    for(int i = 1; i < n; i++) if (rtn > ints[i]) rtn = ints[i];
+    return rtn;
+}
+
+int get_max_a(const int n, const int *ints) {
+    int rtn = ints[0];
+    for(int i = 1; i < n; i++) if (rtn < ints[i]) rtn = ints[i];
+    return rtn;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -41,6 +69,22 @@ int main(int argc, char const *argv[])
     min_heap->print();
     cout << "max_heap dump:" << endl;
     max_heap->print();
+
+    clock_t start;
+    int m;
+    cout << endl << "time test:" << endl;
+    start = clock();
+    m = get_min_h(array_size, array);
+    cout << "min_heap: (" << m << ")" << ((float)(clock() - start))/CLOCKS_PER_SEC << endl;
+    start = clock();
+    m = get_min_a(array_size, array);
+    cout << "min_array: (" << m << ")" << ((float)(clock() - start))/CLOCKS_PER_SEC << endl;
+    start = clock();
+    m = get_max_h(array_size, array);
+    cout << "max_heap: (" << m << ")" << ((float)(clock() - start))/CLOCKS_PER_SEC << endl;
+    start = clock();
+    m = get_max_a(array_size, array);
+    cout << "max_array: (" << m << ")" << ((float)(clock() - start))/CLOCKS_PER_SEC << endl;
 
     delete array;
     delete min_heap;
